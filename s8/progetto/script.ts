5694e604457a67ed62650dc310e554485ab211b3
Nicole Pierdica <hardozone@gmail.com>
2022-03-18 08:25:23 +0100

    progetto editato

--- s8/progetto/script.ts
+++ s8/progetto/script.ts
@@ -6,8 +6,11 @@ fetch('abbigliamento.json')
             
             let vestiario = new Vestiario(item.id, item.codprod, item.collezione, item.capo, item.modello, item.quantita, item.colore, item.prezzoivaesclusa, item.prezzoivainclusa, item.disponibile, item.saldo); // l'istanza va messa fuori dalla classe
             console.log(vestiario);
-            //console.log(vestiario.getSaldoCapo());
+            
             console.log(vestiario.getAcquistoCapo());
+            
+  
+      
         })
     })
 
@@ -47,19 +50,24 @@ class Vestiario {
                  
         
         getAcquistoCapo():string {
-            return `Il prezzo del capo ${this.capo} scontato è ${this.prezzoivainclusa - this.getSaldoCapo()} euro`;
+            return `Il prezzo del capo ${this.capo} scontato è di ${this.prezzoivainclusa - this.getSaldoCapo()} €`;
         }
         
 
-       
+         
       
 }
 
 let pantaloni = new Vestiario(6, 6112, "primavera", "pantaloni", 1242, 7, "verde", 35, 50, "negozio", 30)
 console.log(pantaloni);
+console.log(pantaloni.getAcquistoCapo());
 
 let gonna = new Vestiario(7, 6213, "estate", "gonna", 1229, 4, "bordeaux", 22, 34, "magazzino", 40)
 console.log(gonna);
+console.log(gonna.getAcquistoCapo());
 
 let camicia = new Vestiario(8, 7782, "inverno", "camicia", 1267, 8, "celeste", 40, 55, "online", 50)
-console.log(camicia); 
+console.log(camicia);
+console.log(camicia.getAcquistoCapo());
+
+
